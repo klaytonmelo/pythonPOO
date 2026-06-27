6faa729876dd9bb998d1c47cb624ad3642508ce6
klaytonmelo <klaytonmelo09@gmail.com>
2026-06-27 10:53:17 -0300

    revisando ex018

--- desafios/desafio018/d018.py
+++ desafios/desafio018/d018.py
@@ -11,31 +11,23 @@
 
 from rich import print
 from rich.panel import Panel
-#from rich.traceback import install
-#install()
 
 class Churrasco:
+    #atributos de classe
+    consumo_padrão = 0.400
+    preço_kg = 82.40
+
     def __init__(self, titulo, quant):
-        self.nome = titulo
-        self.quant = quant
+        #atributos de instância
+        self.titulo = titulo
+        self.participantes = quant
 
-    def analisar(self):
-        consumop = 400/1000
-        qtotal = consumop * self.quant
-        ctotal = qtotal * 82.40
-        totalp = ctotal/self.quant
+    def __str__(self):
+        return f"Esse é o {self.titulo} com {self.participantes} pessoas participantes"
+    
+c1 = Churrasco("Churras dos amigos", 15)
+print(c1)
 
-        analise = f"Analisando [green]{self.nome}[/green] com [blue]{self.quant} convidados[/blue]\n"
-        analise += f"cada participante comerá {consumop}Kg de carne\n"
-        analise += f"Recomendo [blue] comprar {qtotal:.2f}kg [/blue]de carne\n"
-        analise += f"ocusto total será de R${ctotal:,.2f}\n"
-        analise += f"cada pessoa pagará R${totalp:,.2f}"
+    
 
-        tabela = Panel(analise ,title=self.nome)
-        print(tabela)
-
-c1 = Churrasco("churras dos amigos ", 15)
-c1.analisar()
-
-c2 = Churrasco("familia", 17)
-c2.analisar()
+    
