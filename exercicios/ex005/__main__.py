9e083bc1f259d50e219b42e97f8af6537193cb46
klaytonmelo <klaytonmelo09@gmail.com>
2026-07-27 19:33:02 -0300

    update ex005!

--- exercicios/ex005/__main__.py
+++ exercicios/ex005/__main__.py
@@ -1,17 +1,21 @@
 from rich import print, inspect
 from classesex005 import Aluno, Professor, Funcionario
 
-a1 = Aluno("José", 17, "informática", "T01")
-a1.fazer_aniversario()
-a1.fazer_matricula()
-#inspect(a1, methods=True)
+def main():
+    a1 = Aluno("José", 17, "informática", "T01")
+    a1.fazer_aniversario()
+    a1.fazer_matricula()
+    #inspect(a1, methods=True)
 
-p1 = Professor("Samuel", 37, "Biologia", "Mestre")
-p1.fazer_aniversario()
-p1.dar_aula()
-#inspect(p1, methods=True)
+    p1 = Professor("Samuel", 37, "Biologia", "Mestre")
+    p1.fazer_aniversario()
+    p1.dar_aula()
+    #inspect(p1, methods=True)
 
-f1 = Funcionario("Claudia", 27, "secretária", "secretaria")
-f1.fazer_aniversario()
-f1.bater_ponto()
-#inspect(f1, methods=True)
+    f1 = Funcionario("Claudia", 27, "secretária", "secretaria")
+    f1.fazer_aniversario()
+    f1.bater_ponto()
+    #inspect(f1, methods=True)
+
+if __name__ == "__main__":
+    main()
